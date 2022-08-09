0eb21d1eea98be4ed2addcb35bfebdc6219d7388
ttmunyunguma <ttmunyunguma@gmail.com>
2022-08-09 10:13:00 +0200

    Initial COmmit

---
+++ src/main/java/com/example/randomapi/RandomApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.randomapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RandomApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RandomApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/randomapi/RandomApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.randomapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RandomApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
